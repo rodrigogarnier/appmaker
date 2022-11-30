8c09d7f580ee01785abbf45312c6d08f2173dccc
rodrigogarnier <118130310+rodrigogarnier@users.noreply.github.com>
2022-11-30 19:12:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+     $('.scroll-up-btn').click(function(){
+         $('html').animate({scrollTop: 0});
+     });
+
+     var typed = new Typed(".typing", {
+         strings:["Aplicativos", "Sites Institucionais", "LandingPages", "Sistemas Web"],
+         typeSpeed:100,
+         backSpeed:60,
+         loop:true
+     });
+     var typed = new Typed(".typing-2", {
+        strings:["Programação", "WebDesigner", "Designer"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    $('.carousel').owlCarousel({
+        margin:20,
+        loop:true,
+        autoplayTimeOut:2000,
+        autoplayHoverPauser:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:false
+            }
+        }
+    });
+});
+
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  // Global settings:
+  
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 900, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
